7174fbc1742c4f528229f8f5b57712cfcdc3a89e
Mizuguchi-Akito <akitoooharagakuenn@gmail.com>
2021-05-11 12:28:47 +0900

    初期設定

---
+++ Main_Top.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HaraTown</title>
+</head>
+<body>
+    <h1>仮置きです。</h1>
+
+    <p class="red">※注意</p>
+    <p class="red">競合を防ぐため、mainブランチにはコミットしないでください。</p>
+</body>
+</html>
