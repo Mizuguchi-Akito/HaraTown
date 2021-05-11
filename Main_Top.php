608bc4338a20b7278bfdda6137d72592c42961f6
Mizuguchi-Akito <akitoooharagakuenn@gmail.com>
2021-05-11 13:30:44 +0900

    初期設定

--- Main_Top.php
+++ Main_Top.php
@@ -7,9 +7,50 @@
     <title>HaraTown</title>
 </head>
 <body>
-    <h1>仮置きです。</h1>
+<main>
+        <article>
+            <section>
+                <table>
+                <h1>HaraTown</h1>
+                    <?php require 'menu.php'; ?>
+                    <?php 
+                    if(!empty($_SESSION['customer'])){
+                        $sql = "select count(id) AS C  from book where customer_id = :customer_id and date <= DATE_SUB(CURRENT_DATE , INTERVAL 7 DAY )";
+                        $stm = $pdo->prepare($sql);
+                        $stm->bindValue(':customer_id', $_SESSION['customer']['id'], PDO::PARAM_STR);
+                        $stm->execute();
+                        $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                        if($result[0]['C'] > 0){
+                            echo '<h2 class="redn">エラー</h2>';
+                        }
+                    }?>
+                    <th colspan="2"></th>
 
-    <p class="red">※注意</p>
-    <p class="red">競合を防ぐため、mainブランチにはコミットしないでください。</p>
+
+                    <?php
+                        //プリペアードステートメントを作る
+                        $sql = 'SELECT * FROM book';
+                        $stm = $pdo->prepare($sql);
+                        $stm->execute();
+                        // $result = fechAll($link, $sql);
+                        $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($result as $row) {
+                            if(!$row['customer_id']){
+                                echo "<br>書籍名 : {$row['name']}" , "<br>";
+                                ?>
+                                <p><img src="images/<?= $row['id'] ?>.png"  class="book_images"></p>
+                                <a href="./datail.php?id=<?= $row['id']?>">詳細</a><br>
+                                <?php
+                                }
+                        }
+                    ?>
+                </table>
+            </section>
+        </article>
+    </main><br>
+    <footer id='footer'>
+        <hr>
+    Copyright c 2021 OharaToshokan All Rights Reserved.
+    </footer>
 </body>
 </html>
