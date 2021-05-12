34f1974bcd7a73b04cdf012340315375c617586d
Kouki-N <K.nakano305@gmail.com>
2021-05-12 14:50:08 +0900

    変更　5/12

---
+++ favorite.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php require 'menu.php'; ?>
+    <?php
+    if (isset($_SESSION['customer'])) {
+    ?>
+        <table>
+            <th>商品番号</th>
+            <th>商品名</th>
+            <th>価格</th>
+            <th>ブランド名</th>
+            <th>カラー</th>
+            <th>サイズ</th>
+            <?php
+            require 'db_connect.php';
+            $sql = "select * from favorite, product where customer_id = :customer_id and product_id = id";
+            $stm = $pdo->prepare($sql);
+            $stm->bindValue(':customer_id', $_SESSION['customer']['id'], PDO::PARAM_STR);
+            $stm->execute();
+            $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($result as $row) {
+                $id = $row['id'];
+            ?>
+                <tr>
+                    <td><?= $id ?></td>
+                    <td><a href="detail.php?id=<?= $id ?>"><?= $row['name'] ?></a></td>
+                    <td><?= $row['price'] ?></td>
+                    <td><a href="favorite_delete.php?id=<?= $id ?>">削除</a></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    <?php
+    } else {
+    ?>
+        お気に入りを表示するには、ログインしてください。
+    <?php
+    }
+    ?>
+</body>
+
+</html>
---
+++ favorite_insert.php
@@ -0,0 +1,31 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>お気に入り画面</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php require 'menu.php'; ?>
+    <?php
+    if (isset($_SESSION['customer'])) {
+        require 'db_connect.php';
+        $sql = "insert into favorite values(:customer_id,:product_id)";
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':customer_id', $_SESSION['customer']['id'], PDO::PARAM_STR);
+        $stm->bindValue(':product_id', $_REQUEST['id'], PDO::PARAM_STR);
+        $stm->execute();
+    ?>
+        お気に入りに商品を追加しました。
+        <hr>
+    <?php require 'favorite.php';
+    } else {
+    ?>
+        お気に入りに商品を追加するには、ログインしてください。
+    <?php
+    }
+    ?>
+</body>
+</html>
