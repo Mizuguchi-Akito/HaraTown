54cbe590b52320bbd123ab5ad8c5cfca99d23a63
Kouki-N <K.nakano305@gmail.com>
2021-05-14 10:54:20 +0900

    変更　5/14

--- favorite.php
+++ favorite.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+    <meta charset="UTF-8">
+    <title>お気に入り画面</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
 <body>
     <?php require 'menu.php'; ?>
     <?php
