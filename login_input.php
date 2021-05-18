9329c1d4c1b35eb4020fe49b459c9d8382b94723
utsugi <k.utsugi021@gmail.com>
2021-05-18 12:21:45 +0900

    ログイン画面

--- login_input.php
+++ login_input.php
@@ -5,13 +5,27 @@
 <head>
     <meta charset="UTF-8">
     <title>ログイン</title>
-</head>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
 
+</head>
 <body>
-    <form action="login_output.php" method="post">
-        ログインID<input type="text" name="login"><br>
-        パスワード<input type="password" name="password"><br>
-        <input type="submit" value="ログイン">
+    <div class="login">
+    <h2 class="active">Login</h2>
+    <form action="login_output.php" method="post" class="login_class">
+        <input type="text" class="login_text" name="login">
+     <span class="login_span">LoginID</span><br>
+        <input type="password" class="login_text" name="password">
+    <span class="login_span">password</span>
+    <br>
+
+    
+        
+        
+    <button class="signin">
+      Login
+    </button>
     </form>
-</body>
+    </div>
+
 </html>
