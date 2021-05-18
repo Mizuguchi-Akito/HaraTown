2ce468f6a3e87c4f27a97b6527e81b84f96c43fd
Kouki-N <K.nakano305@gmail.com>
2021-05-18 12:07:46 +0900

    履歴

--- history.php
+++ history.php
@@ -5,21 +5,75 @@
 <head>
 	<meta charset="UTF-8">
 	<title>購入履歴画面</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-	<?php require 'menu.php'; ?>
 	<?php
-	//ログインの確認
-	if (!isset($_SESSION['customer'])) {
-		//MySQLデータベースに接続する
-		require 'db_connect.php';
-	} else {
+	require 'menu.php';
+	require 'db_connect.php';
+
+	if (isset($_SESSION['customer'])) {
+		$sql = "select * from purchase where customer_id = :id";
+		$stm = $pdo->prepare($sql);
+		$stm->bindValue(':id', $_SESSION['customer']['id'], PDO::PARAM_INT);
+		$stm->execute();
+		$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+		foreach ($result as $row) {
+			$sql2 = "
+				select product.id as product_id, name, price, count from purchase_detail, product where purchase_id = :purchase_id and product_id = product.id ";
+			$stm2 = $pdo->prepare($sql2);
+			$stm2->bindValue(':purchase_id', $row['id'], PDO::PARAM_INT);
+			$stm2->execute();
+			$result2 = $stm2->fetchAll(PDO::FETCH_ASSOC);
+			$size = '';
 	?>
-		購入履歴を表示するにはログインしてください。
+
+
+			<table>
+				<tr>
+					<th>商品名</th>
+					<th>価格</th>
+					<th>サイズ</th>
+					<th>個数</th>
+					<th>小計</th>
+				</tr>
+
+				<?php
+				$total = 0;
+				foreach ($result2 as $row2) {
+					$subTotal = $row2['price'] * $row2['count'];
+					$total += $subTotal;
+				?>
+
+					<tr>
+						<td><a href="detail.php?id=<?= $row2['product_id'] ?>"><?= $row2['name'] ?></a></td>
+						<td><?= $row2['price'] ?></td>
+						<td><?= $row2['size'] ?></td>
+						<td><?= $row2['count'] ?></td>
+						<td><?= $subTotal ?></td>
+					</tr>
+
+				<?php
+				}
+				?>
+
+				<td>合計</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td><?= $total; ?></td>
+
+			</table>
+			<hr>
+
 	<?php
+		}
+	} else {
+		echo "購入履歴を表示するには、ログインしてください。";
 	}
 	?>
 </body>
+
 </html>
