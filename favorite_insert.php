2c8fe3f70d96d33ee1b660148c6a2d8eddcb1a6e
Mizuguchi-Akito <akitoooharagakuenn@gmail.com>
2021-05-18 16:13:16 +0900

    変更 5/18

--- favorite_insert.php
+++ favorite_insert.php
@@ -14,7 +14,26 @@
 		//MySQLデータベースに接続する
 		require 'db_connect.php';
 
-		//SQL文を作る（プレースホルダを使った式）
+        //SQL文を作る（プレースホルダを使った式）
+        $sql = "select count(customer_id) from favorite where customer_id = :customer_id and product_id = :product_id group by product_id ";
+        $stm = $pdo->prepare($sql);
+        // echo 'ok';
+		//プリペアードステートメントに値をバインドする
+		$stm->bindValue(':customer_id', $_SESSION['customer']['id'], PDO::PARAM_STR);
+		$stm->bindValue(':product_id', $_REQUEST['id'], PDO::PARAM_STR);
+        //SQL文を実行する
+        $stm->execute();
+        $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($result);
+
+        foreach($result as $item){
+           if($item >= 1){
+               $ok = false;
+            }else{
+                $ok = true;
+            }
+        }
+        if($ok !== false){
 		$sql = "insert into favorite values(:customer_id,:product_id)";
 		//プリペアードステートメントを作る
 		$stm = $pdo->prepare($sql);
@@ -22,9 +41,13 @@
 		$stm->bindValue(':customer_id', $_SESSION['customer']['id'], PDO::PARAM_STR);
 		$stm->bindValue(':product_id', $_REQUEST['id'], PDO::PARAM_STR);
 		//SQL文を実行する
-		$stm->execute();
+        $stm->execute();
+
+        echo "お気に入りの商品を追加しました。";
+    }else{
+        echo "すでに登録されています";
+    }
 	?>
-		お気に入りに商品を追加しました。
 		<hr>
 	<?php require 'favorite.php';
 	} else {
